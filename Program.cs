131ec3ffaca0623708bf8aa84bbb95666e6b69ef
MariiaPiven <piven_ak24@nuwm.edu.ua>
2025-11-25 15:05:03 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,79 +1,10 @@
 using System;
-using System.Text.Json;
-using System.Xml.Linq;
+using JsonToXmlAdapterExample.Contracts;
+using JsonToXmlAdapterExample.Adapters;
+using JsonToXmlAdapterExample.Options;
 
 namespace JsonToXmlAdapterExample
 {
-    // ==== Target ====
-    public interface IConverter
-    {
-        string Convert(string input);
-    }
-
-    // ==== Adapter ====
-    public class JsonToXmlAdapter : IConverter
-    {
-        public string Convert(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-                throw new ArgumentException("Input JSON is empty", nameof(input));
-
-            using var doc = JsonDocument.Parse(input);
-
-            var rootElement = new XElement("Root");
-            ConvertElement(doc.RootElement, rootElement);
-
-            var xdoc = new XDocument(rootElement);
-            return xdoc.ToString();
-        }
-
-        private void ConvertElement(JsonElement element, XElement parent)
-        {
-            switch (element.ValueKind)
-            {
-                case JsonValueKind.Object:
-                    foreach (var prop in element.EnumerateObject())
-                    {
-                        var child = new XElement(prop.Name);
-                        parent.Add(child);
-                        ConvertElement(prop.Value, child);
-                    }
-                    break;
-
-                case JsonValueKind.Array:
-                    foreach (var item in element.EnumerateArray())
-                    {
-                        var itemEl = new XElement("Item");
-                        parent.Add(itemEl);
-                        ConvertElement(item, itemEl);
-                    }
-                    break;
-
-                case JsonValueKind.String:
-                    parent.Value = element.GetString();
-                    break;
-
-                case JsonValueKind.Number:
-                    parent.Value = element.GetRawText();
-                    break;
-
-                case JsonValueKind.True:
-                case JsonValueKind.False:
-                    parent.Value = element.GetBoolean().ToString();
-                    break;
-
-                case JsonValueKind.Null:
-                    parent.Value = string.Empty;
-                    break;
-
-                default:
-                    parent.Value = element.GetRawText();
-                    break;
-            }
-        }
-    }
-
-    // ==== Client Code ====
     class Program
     {
         static void Main()
@@ -82,12 +13,22 @@ namespace JsonToXmlAdapterExample
                 ""name"": ""Maria"",
                 ""age"": 20,
                 ""isStudent"": true,
-                ""scores"": [10, 9, 8]
+                ""extra"": null,
+                ""scores"": [10, 9, null, 8]
             }";
+
+            // Configure conversion behavior
+            var options = new ConverterOptions
+            {
+                RootName = "Person",
+                ArrayItemName = "Score",
+                NullHandling = NullHandling.EmptyElement,
+                BooleanHandling = BooleanHandling.Lowercase
+            };
 
             IConverter converter = new JsonToXmlAdapter();
 
-            string xml = converter.Convert(json);
+            string xml = converter.Convert(json, options);
 
             Console.WriteLine("=== XML Result ===");
             Console.WriteLine(xml);
