6250d9dd6b5c8547eefd93de94db5be06d2ce9b2
MariiaPiven <piven_ak24@nuwm.edu.ua>
2025-11-25 13:16:19 +0200

    Implement PR agent workflow in Program.cs
    Added GitHub Actions configuration for PR agent.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
+name: PR Agent for NUWM
 
-namespace LabWork
-{
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+on:
+  pull_request:
+    types: [ opened, reopened, synchronize, edited ]
+
+jobs:
+  pr_agent_job:
+    runs-on: ubuntu-latest
+    name: Run pr agent on every pull request
+    steps:
+      - name: pr-agent-nuwm
+        uses: EzGrade/Pr-Agent-NUWM@main
+        env:
+          GIT_APP_ID: ${{ secrets.GIT_APP_ID }}
+          GIT_PRIVATE_KEY: ${{ secrets.GIT_PRIVATE_KEY }}
+          GIT_INSTALLATION_ID: ${{ secrets.GIT_INSTALLATION_ID }}
+          GIT_REPOSITORY: ${{ github.repository }}
+          OPENAI_API_KEY: ${{ secrets.OPENAI_API_KEY }}
+          OPENAI_MODEL: ${{ secrets.OPENAI_MODEL }}
+          GOOGLE_CREDENTIALS_CONTENT: ${{ secrets.GOOGLE_CREDENTIALS_CONTENT }}
+          GOOGLE_SPREADSHEET_URL: ${{ secrets.GOOGLE_SPREADSHEET_URL }}
+          GOOGLE_SHEETS_NAMING: ${{ secrets.GOOGLE_SHEETS_NAMING }}
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
