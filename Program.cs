02e19aebaf5ebad9c3705fa18990c3a3cacbae34
MariiaPiven <piven_ak24@nuwm.edu.ua>
2025-11-25 13:35:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,62 +1,41 @@
 using System;
-using System.Text.Json;
-using System.Xml.Linq;
+using LabWork.Adapters;
 
-namespace LabWork.Adapters
+namespace LabWork
 {
-    // Адаптер який реалізує IConverter і перетворює JSON у XML
-    public class JsonToXmlAdapter : IConverter
+    // Даний проект є шаблоном для виконання лабораторних робіт
+    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
+    // Необхідно змінювати і дописувати код лише в цьому проекті
+    // Відео-інструкції щодо роботи з github можна переглянути 
+    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    class Program
     {
-        public string Convert(string input)
+        static void Main(string[] args)
         {
-            if (string.IsNullOrWhiteSpace(input))
-                throw new ArgumentException("Input JSON is empty", nameof(input));
+            // Демонстрація використання адаптера (JSON -> XML)
+            string sampleJson = @"{
+  ""person"": {
+    ""name"": ""Maria Piven"",
+    ""age"": 24,
+    ""phones"": [""+380501234567"", ""+380671112233""],
+    ""address"": {
+      ""city"": ""Kyiv"",
+      ""street"": ""Main St""
+    }
+  }
+}";
 
-            using var doc = JsonDocument.Parse(input);
+            IConverter converter = new JsonToXmlAdapter();
 
-            var rootElement = new XElement("Root");
-            ConvertElement(doc.RootElement, rootElement);
-
-            var xdoc = new XDocument(rootElement);
-            return xdoc.ToString();
-        }
-
-        private void ConvertElement(JsonElement element, XElement parent)
-        {
-            switch (element.ValueKind)
+            try
             {
-                case JsonValueKind.Object:
-                    foreach (var prop in element.EnumerateObject())
-                    {
-                        var child = new XElement(prop.Name);
-                        parent.Add(child);
-                        ConvertElement(prop.Value, child);
-                    }
-                    break;
-                case JsonValueKind.Array:
-                    foreach (var item in element.EnumerateArray())
-                    {
-                        var itemEl = new XElement("Item");
-                        parent.Add(itemEl);
-                        ConvertElement(item, itemEl);
-                    }
-                    break;
-                case JsonValueKind.String:
-                    parent.Value = element.GetString();
-                    break;
-                case JsonValueKind.Number:
-                    parent.Value = element.GetRawText();
-                    break;
-                case JsonValueKind.True:
-                case JsonValueKind.False:
-                    parent.Value = element.GetBoolean().ToString();
-                    break;
-                case JsonValueKind.Null:
-                    parent.Value = string.Empty;
-                    break;
-                default:
-                    parent.Value = element.GetRawText();
-                    break;
+                string xml = converter.Convert(sampleJson);
+                Console.WriteLine("Converted XML:\n");
+                Console.WriteLine(xml);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Conversion failed: " + ex.Message);
             }
         }
     }
